57416425074e34bf58c0b295bc010fa3858a99fe
YoheiKakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-11-06 19:10:15 +0900

    update camera function/sample

--- src/camera_sample.py
+++ src/camera_sample.py
@@ -53,3 +53,22 @@ len(hand_landmarks0) ## num of landmark
 landmark = hand_landmarks0[0]
 
 print(landmark.x, landmark.y, landmark.z)
+
+##
+## using calibration parameters
+##
+camera_matrix = np.array([[630.408201, 0.000000, 408.395562],
+                          [0.000000, 628.574130, 296.839336],
+                          [0.000000, 0.000000, 1.000000]], dtype='float64')
+distortion = np.array([0.018656, 0.034678, 0.002943, -0.004527, 0.000000], dtype='float64')
+projection_matrix = np.array([[647.145834, 0.000000, 404.360305, 0.000000],
+                              [0.000000, 647.276761, 298.733318, 0.000000],
+                              [0.000000, 0.000000, 1.000000, 0.000000]], dtype='float64')
+#
+cap = camera_capture.CvCapture(-1) ## create instance without opening camera device
+cap.set_camera_parameters(camera_matrix=camera_matrix, distortion=distortion, projection_matrix=projection_matrix)
+img_und = cap.undistort_image(img) ## undistort image
+## cap.undistort_points(2d_points)
+## cap.project_points(3d_points)
+## cap.project_points_new(3d_points)
+## cap.unproject_points(2d_points, depth_list=...)
--- src/mppack/camera_capture.py
+++ src/mppack/camera_capture.py
@@ -21,6 +21,14 @@ class CvCapture(object):
         self.mapxy = None
 
     def set_camera_parameters(self, distortion=None, camera_matrix=None, projection_matrix=None, new_camera_matrix=None):
+        """Setting camera parameters
+
+        Args:
+            distortion (numpy.array, optional) : 1x5 vector, distortion parameters
+            camera_matrix (numpy.array, optional) : 3x3 matrix, camera original matrix
+            projection_matrix (numpy.array, optional) : 3x4 matrix, projection matrix of undistorted image
+            new_camera_matrix (numpy.array, optional) : 3x3 matrix, camera matrix of undistorted image
+        """
         self.distortion = distortion
         self.camera_matrix = camera_matrix
         self.projection_matrix = projection_matrix
@@ -33,21 +41,60 @@ class CvCapture(object):
                                                      self.new_camera_matrix, (self.width, self.height), cv2.CV_32FC1)
 
     def undistort_points(self, points):
+        """Undistorting points on image
+
+        Args:
+            points ( list[numpy.array] ) : list of 2D points on original image
+
+        Retuns:
+            numpy.array : 2D points in undistorted image
+        """
         return cv2.undistortPoints(np.array(points), self.camera_matrix, self.distortion, P=self.new_camera_matrix)
 
     def undistort_image(self, img):
+        """Generating undistorted image
+
+        Args:
+            img ( numpy.array ) : Original image
+        Retuns:
+            numpy.array : Undistorted image
+
+        """
         if self.mapxy is not None:
             return cv2.remap(img, self.mapxy[0], self.mapxy[1], cv2.INTER_AREA)
 
     def project_points(self, points):
+        """Projecting 3D points to the original image plane
+
+        Args:
+            points( list [numpy.array] ) : List of 3D points (camera coordinates)
+
+        Retuns:
+            numpy.array : Projected 2D points
+        """
         if self.camera_matrix is not None and self.distortion is not None:
-            return cv2.projectPoints(points, self.zero3, self.zero3, self.camera_matrix, self.distortion)
+            return cv2.projectPoints(np.array(points), self.zero3, self.zero3, self.camera_matrix, self.distortion)
 
     def project_points_new(self, points):
+        """Projecting 3D points to undistorted image plane
+
+        Args:
+            points( list [numpy.array] ) : List of 3D points (camera coordinates)
+
+        Retuns:
+            numpy.array : Projected 2D points
+        """
         if self.new_camera_matrix is not None:
             return cv2.projectPoints(points, self.zero3, self.zero3, self.new_camera_matrix, self.zero_dist)
 
     def unproject_points(self, uv_list, depth=1.0, depth_list=None):
+        """Unproject 2D points to 3D points using given depth
+
+        Args:
+            uv_list ( list[numpy.array] ) : List of 2D points (undistorted image plane)
+            depth (float, default=1.0) : Depth for using unproject
+            depth_list (list[float], optional) : Depth for using unproject, corresponding to each point
+        """
         cmat = self.new_camera_matrix
         fx = cmat[0][0]
         fy = cmat[1][1]
